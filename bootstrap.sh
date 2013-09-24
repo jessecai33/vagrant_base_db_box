d9b9d59aa570a28377796d8516a6ca9c9c0d4c36
Jesse Cai <mojacc@gmail.com>
2013-09-24 16:34:06 +1000

    Removed sudo since provisioner runs as root.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,28 +2,28 @@
 
 # Ubuntu mysql box provision
 
-sudo apt-get -y update
+apt-get -y update
 
-#local time
-sudo ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+# local time
+ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
 # ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
 
-#git
-sudo apt-get -y install git-core
+# git
+apt-get -y install git-core
 
-#mariaDB
-sudo apt-get -y install python-software-properties
-sudo apt-get -y install software-properties-common
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
-sudo add-apt-repository 'deb http://mirror.aarnet.edu.au/pub/MariaDB/repo/5.5/ubuntu precise main'
-sudo apt-get -y update
-sudo apt-get -y install mariadb-server
+# MySQL
+apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+# will be prompted with password
+
+mysql_install_db
+
+/usr/bin/mysql_secure_installation
 
 #install sysv-rc-conf (Ubuntu version of chkconfig)
-sudo apt-get -y install sysv-rc-conf
+apt-get -y install sysv-rc-conf
 
 # autostarg mysql
-sudo sysv-rc-conf mysqld on
+sysv-rc-conf mysqld on
 
 # start mysql
-sudo service mysqld restart
+service mysqld restart
